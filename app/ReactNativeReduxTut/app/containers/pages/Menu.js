fb77833d00b73967b712f7d7baba7a9984203248
Unknown <murugappanviswanathan@gmail.com>
2018-02-02 10:38:59 +0530

    App - Added collapsible library

--- app/ReactNativeReduxTut/app/containers/pages/Menu.js
+++ app/ReactNativeReduxTut/app/containers/pages/Menu.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {ScrollView, View, TextInput, Image, TouchableHighlight, StyleSheet, Text} from "react-native";
 import {connect} from 'react-redux';
 import Icon from 'react-native-vector-icons/Entypo';
+import Accordion from 'react-native-collapsible/Accordion';
 
 class Menu extends Component {
     static navigationOptions = {
@@ -11,11 +12,57 @@ class Menu extends Component {
         ),
     };
 
+    MockData = [
+        {
+            header: 'sectionHeader1',
+            member: [
+                {
+                    title: 'memberTitle1',
+                    content: 'content',
+                },
+            ]
+        },
+        {
+            header: 'sectionHeader2',
+            member: [
+                {
+                    title: 'memberTitle2',
+                    content: 'content',
+                },
+            ]
+        },
+        {
+            header: 'sectionHeader3',
+            member: [
+                {
+                    title: 'memberTitle3',
+                    content: 'content',
+                },
+            ]
+        },
+    ]
+
+
+    _renderContent = (section) => <Text>{section.member[0].title}</Text>;
+    _renderHeader = (section)  => <Text>{section.header}</Text>;
+
     render() {
-        return <View>
-            <Text>Dummy line </Text>
-            <Text>Menu </Text>
-        </View>        
+        return (
+        // <ExpanableList style={{paddingLeft: 5, paddingTop: 5, paddingRight: 5, paddingBottom: 5, backgroundColor: 'white'}}
+        //     dataSource={this.MockData}
+        //     headerKey="header"
+        //     memberKey="member"
+        //     renderRow={this._renderRow}
+        //     renderSectionHeaderX={this._renderSection}
+        //     // openOptions={[1,2,]}
+        // />
+        <Accordion
+        sections={this.MockData}
+        renderHeader={this._renderHeader}
+        renderContent={this._renderContent}
+        initiallyActiveSection={1}
+      />
+        );
     }
 }
 
